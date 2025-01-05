7ded3353e6f0f3262bc648d964a42dc8d649116f
Damian Bednarek <damian.bednarekk@gmail.com>
2025-01-05 17:28:51 +0100

    Basic funcionallity of the app

---
+++ main.py
@@ -0,0 +1,53 @@
+import openmeteo_requests
+import pandas as pd
+import requests_cache
+import streamlit as st
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
+retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+openmeteo = openmeteo_requests.Client(session=retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+
+current_date = pd.Timestamp.now().strftime("%Y-%m-%d")
+first_day_of_month = pd.Timestamp.now().replace(day=2).strftime("%Y-%m-%d")
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 51.8197,
+    "longitude": 19.3038,
+    "daily": ["temperature_2m_max", "temperature_2m_min"],
+    "timezone": "auto",
+    "start_date": first_day_of_month,
+    "end_date": current_date,
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+
+daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+
+
+daily_data = {
+    "data": pd.date_range(
+        start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+        end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+        freq=pd.Timedelta(seconds=daily.Interval()),
+        inclusive="left",
+    ).strftime("%Y-%m-%d"),
+}
+
+# daily_data["Maksymalna temperatura"] = f"{daily_temperature_2m_max.round(1)} °C"
+daily_data["Maksymalna temperatura °C"] = daily_temperature_2m_max.round(1)
+daily_data["Kolor"] = "test"
+
+
+daily_dataframe = pd.DataFrame(data=daily_data)
+st.write(daily_dataframe)
